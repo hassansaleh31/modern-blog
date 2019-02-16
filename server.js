e0083154ed01b9acb78c54b2ba6bfb1e7841bdb7
Hassan Saleh <hassansaleh31@gmail.com>
2019-02-17 00:27:07 +0200

    Added web server to docker-compose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-require('dotenv').config()
 const express = require('express')
 const next = require('next')
+
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config()
+}
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
